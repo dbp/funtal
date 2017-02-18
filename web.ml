d60028fcd5f216353ff97ad21096ffd883227fa7
Daniel Patterson <dbp@dbpmail.net>
2017-02-18 17:02:07 -0500

    js: WIP commit with javascript errors from js_of_ocaml runtime

--- web.ml
+++ web.ml
@@ -1,3 +1,8 @@
+open Examples;;
+
 module H = Dom_html
 
-let () = H.window##alert (Js.string "hello, world")
+
+let () =
+  let e = higher_order in
+  (H.getElementById "input")##.innerHTML := Js.string (Ftal.F.show_exp e)
