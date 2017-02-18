50e6a9b39a6ed755d11ae7182f1c9134051bed30
Gabriel Scherer <gabriel.scherer@gmail.com>
2017-02-18 13:07:11 -0500

    enable located parsing error messages
        $ ./driver.native test.ftal
        File "test.ftal", line 5, characters 8-9, parsing error:
        >   [], l -> <1>

--- parse.ml
+++ parse.ml
@@ -1,3 +1,52 @@
+(** Parsing error reporting *)
+
+let position {Lexing.pos_fname; pos_lnum; pos_cnum; pos_bol} =
+  let file = pos_fname in
+  let line = pos_lnum in
+  let character = pos_cnum - pos_bol in
+  (file, line, character)
+
+let nth_line file line =
+  try
+    let input = open_in file in
+    for i = 1 to line - 1 do
+      ignore (input_line input)
+    done;
+    let result = input_line input in
+    close_in input;
+    Some result
+  with _ -> None
+
+let report_error lexbuf = function
+  | Parser.Error ->
+    let file, start_line, start_character = position lexbuf.Lexing.lex_start_p in
+    let _, curr_line, curr_character = position lexbuf.Lexing.lex_curr_p in
+    let open Printf in
+    let lines =
+      if curr_line = start_line
+      then sprintf "line %d" curr_line
+      else sprintf "lines %d-%d" start_line curr_line in
+    let characters =
+      if curr_line = start_line
+      then sprintf "characters %d-%d" start_character curr_character
+      else sprintf "character %d" start_character in
+    Printf.eprintf "File %S, %s, %s, parsing error:\n%!"
+      file lines characters;
+    begin match nth_line file curr_line with
+      | None -> ()
+      | Some line -> Printf.eprintf "> %s\n" line;
+    end;
+  | Lexer.Error (invalid_input, err_pos) ->
+    let file, line, character = position err_pos in
+    Printf.eprintf
+      "File %S, line %d, character %d, lexing error:\n" file line character;
+    begin match nth_line file line with
+      | None -> ()
+      | Some line -> Printf.eprintf "> %s\n" line;
+    end;
+    Printf.eprintf "Invalid input %S\n%!" invalid_input
+  | exn -> raise exn
+
 (** Parsing functions *)
 
 let parse parse_fun lexbuf =
@@ -21,59 +70,8 @@ let parse_file parse_fun path =
     lexbuf.lex_curr_p <- lexbuf.lex_start_p;
     lexbuf
   in
-  parse parse_fun lexbuf
-
-(** Parsing error reporting *)
-
-let position {Lexing.pos_fname; pos_lnum; pos_cnum; pos_bol} =
-  let file = pos_fname in
-  let line = pos_lnum in
-  let character = pos_cnum - pos_bol in
-  (file, line, character)
-
-let nth_line file line =
   try
-    let input = open_in file in
-    for i = 1 to line - 1 do
-      ignore (input_line input)
-    done;
-    let result = input_line input in
-    close_in input;
-    Some result
-  with _ -> None
-
-let report_error = function
-(*
-  | Parsing (message, start_pos, end_pos) ->
-    let file, start_line, start_character = position start_pos in
-    let _, curr_line, curr_character = position end_pos in
-    let open Printf in
-    let lines =
-      if curr_line = start_line
-      then sprintf "line %d" curr_line
-      else sprintf "lines %d-%d" start_line curr_line in
-    let characters =
-      if curr_line = start_line
-      then sprintf "characters %d-%d" start_character curr_character
-      else sprintf "character %d" start_character in
-    Printf.eprintf "File %S, %s, %s, parsing error:\n%!"
-      file lines characters;
-    begin match nth_line file curr_line with
-      | None -> ()
-      | Some line -> Printf.eprintf "> %s\n" line;
-    end;
-    begin match message with
-    | None -> ()
-    | Some error_message -> prerr_endline error_message
-    end
-*)
-  | Lexer.Error (invalid_input, err_pos) ->
-    let file, line, character = position err_pos in
-    Printf.eprintf
-      "File %S, line %d, character %d, lexing error:\n" file line character;
-    begin match nth_line file line with
-      | None -> ()
-      | Some line -> Printf.eprintf "> %s\n" line;
-    end;
-    Printf.eprintf "Invalid input %S\n%!" invalid_input
-  | exn -> raise exn
+    parse parse_fun lexbuf
+  with exn ->
+    report_error lexbuf exn;
+    exit 1
