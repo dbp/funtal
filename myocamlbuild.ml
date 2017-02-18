e00a64c4008faaeed3e6f5949604c97702bf0821
Daniel Patterson <dbp@dbpmail.net>
2017-02-18 16:05:54 -0500

    js: hello world working

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -5,3 +5,12 @@ open Ocamlbuild_plugin
 let () = flag ["menhir"; "parser"; "trace"] (A"--trace")
 
 let () = dispatch dispatch_default
+
+let _ =
+       Ocamlbuild_plugin.dispatch
+         (fun hook ->
+            dispatch_default hook;
+            Ocamlbuild_js_of_ocaml.dispatcher
+              ~oasis_executables:["web.byte"]
+              hook;
+         )
---
+++ web.ml
@@ -0,0 +1,3 @@
+module H = Dom_html
+
+let () = H.window##alert (Js.string "hello, world")
