7760284009e25d475e5eab8f30646ae5d4d556fe
Daniel Patterson <dbp@dbpmail.net>
2017-02-18 18:33:50 -0500

    js: add basic stepper to UI

--- web.ml
+++ web.ml
@@ -3,7 +3,28 @@ open Examples;;
 module H = Dom_html
 
 let _ =
-  let e = higher_order in
-  let es = Ftal.F.show_exp e in
+  let e = ref higher_order in
+  let m = ref ([],[],[]) in
+  let es = Ftal.F.show_exp !e in
+  let set_text i t =
+    let open H in
+    (getElementById i)##.innerHTML := Js.string t
+  in
+  let refresh _ =
+    let (h,r,s) = !m in
+    let _ = set_text "registers" (Ftal.TAL.show_regm r) in
+    let _ = set_text "stack" (Ftal.TAL.show_stackm s) in
+    let _ = set_text "heap" (Ftal.TAL.show_heapm h) in
+    let _ = set_text "program" (Ftal.F.show_exp !e) in
+    ()
+  in
+  let next _ =
+    let (nm,ne) = Ftal.F.step (!m, !e) in
+    m := nm;
+    e := ne;
+    refresh ();
+    Js._false
+  in
+  let _ = H.((getElementById "next")##.onclick := (H.handler next)) in
   Js.Opt.bind(H.(CoerceTo.textarea (getElementById "input")))
     (fun e -> e##.value := Js.string es; Js.Opt.return ())
