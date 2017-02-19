872f84b050e32ceabb2c1cfc4958c5c32c4048d1
Daniel Patterson <dbp@dbpmail.net>
2017-02-18 21:12:41 -0500

    js: add loading from editor (has to be F at top for now)

--- web.ml
+++ web.ml
@@ -2,9 +2,16 @@ open Examples;;
 
 module H = Dom_html
 
+let p1 = {|FT [int, ?] (
+     mv r1, 1;
+     add r1, r1, 1;
+     halt int, * {r1}
+;
+     []
+)|}
+
 let _ =
   let hist = ref ((higher_order, ([],[],[])), []) in
-  let es = Ftal.F.show_exp (fst (fst !hist)) in
   let set_text i t =
     let open H in
     (getElementById i)##.innerHTML := Js.string t
@@ -34,6 +41,20 @@ let _ =
     then ()
     else hist := ((ne,nm), (e,m)::rest)
   in
+  let load _ =
+    let open H in
+    let _ = Js.Opt.bind
+      (CoerceTo.textarea (getElementById "input"))
+      (fun p ->
+         let s = Js.to_string p##.value in
+         let _ = print_endline s in
+         let e = Parse.parse_string Parser.f_expression_eof s in
+         let _ = Ftal.(FTAL.tc (FTAL.default_context TAL.QOut) (FTAL.FC e)) in
+         hist := ((e, ([],[],[])), []);
+         refresh ();
+         Js.Opt.return Js._false
+      ) in Js._false
+  in
   let next _ =
     next' ();
     refresh ();
@@ -51,8 +72,9 @@ let _ =
     refresh ();
     Js._false
   in
+  let _ = H.((getElementById "load")##.onclick := (H.handler load)) in
   let _ = H.((getElementById "next")##.onclick := (H.handler next)) in
   let _ = H.((getElementById "prev")##.onclick := (H.handler prev)) in
   let _ = H.((getElementById "many")##.onclick := (H.handler many)) in
   Js.Opt.bind(H.(CoerceTo.textarea (getElementById "input")))
-    (fun e -> e##.value := Js.string es; Js.Opt.return ())
+    (fun e -> e##.value := Js.string p1; Js.Opt.return ())
