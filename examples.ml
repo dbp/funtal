5da31793eb51bd968f606431ea8762a751a8e504
Gabriel Scherer <gabriel.scherer@gmail.com>
2017-02-20 14:22:40 -0500

    more syntaxed examples

--- examples.ml
+++ examples.ml
@@ -67,70 +67,40 @@ lam (x:int).
 |}
 (* Different number of basic blocks *)
 
-let blocks_1 =
-  let l = FTAL.gen_sym ~pref:"l" () in
-  let h = [(l,
-            TAL.(Box, HCode ([DZeta "z3"; DEpsilon "e"],
-                        [("ra", TBox (PBlock ([], [("r1", TInt)], SAbstract ([], "z3"), QEpsilon "e")))],
-                        SAbstract ([TInt], "z3"),
-                        QR "ra",
-                        [Isld (dummy_loc, "r1", 0);
-                         Iaop (dummy_loc, Add, "r1", "r1", UW (dummy_loc, WInt (dummy_loc, 1)));
-                         Iaop (dummy_loc, Add, "r1", "r1", UW (dummy_loc, WInt (dummy_loc, 1)));
-                         Isfree (dummy_loc, 1);
-                         Iret (dummy_loc, "ra", "r1")])))] in
-  F.(ELam (dummy_loc, [("x", TInt)],
-           EApp (dummy_loc, EBoundary
-                   (dummy_loc, TArrow ([TInt], TInt),
-                    None,
-                    (TAL.(dummy_loc, [Iprotect (dummy_loc, [], "z2");
-                           Imv (dummy_loc, "r1", UW (dummy_loc, WLoc (dummy_loc, l)));
-                           Ihalt (dummy_loc, FTAL.tytrans (TArrow ([TInt], TInt)),
-                                  SAbstract ([], "z2"),
-                                  "r1")],
-                          h))),
-                 [EVar (dummy_loc, "x")])))
+let blocks_1 = Parse.parse_string Parser.f_expression_eof {|
+lam (x:int).
+  FT[(int) -> int, ?]
+    ([protect , z2;
+      mv r1, l2;
+      halt
+        box forall[z3, e4].
+          {ra : box forall[].{r1 : int; z3} e4;
+           int :: z3} ra,
+        z2 {r1}],
+      [l2 -> box code [z3, e]{ra : box forall[].{r1 : int; z3} e;
+                             int :: z3} ra.
+               [sld r1, 0; add r1, r1, 1; add r1, r1, 1; sfree 1; ret ra {r1}]])
+    x
+|}
 
-
-let blocks_2 =
-  let l1 = FTAL.gen_sym ~pref:"l" () in
-  let l2 = FTAL.gen_sym ~pref:"l" () in
-  let h = TAL.([(l1,
-                 (Box, HCode ([DZeta "z3"; DEpsilon "e1"],
-                        [("ra", TBox (PBlock ([],
-                                              [("r1", TInt)],
-                                              SAbstract ([], "z3"),
-                                              QEpsilon "e1")))],
-                        SAbstract ([TInt], "z3"),
-                        QR "ra",
-                        [Isld (dummy_loc, "r1", 0);
-                         Iaop (dummy_loc, Add, "r1", "r1", UW (dummy_loc, WInt (dummy_loc, 1)));
-                         Isst (dummy_loc, 0, "r1");
-                         Ijmp (dummy_loc, UApp (dummy_loc, UW (dummy_loc, WLoc (dummy_loc, l2)), [OS (SAbstract ([], "z3"));
-                                                    OQ (QEpsilon "e1")]))])));
-                (l2,
-                 (Box, HCode ([DZeta "z4"; DEpsilon "e2"],
-                        [("ra", TBox (PBlock ([],
-                                              [("r1", TInt)],
-                                              SAbstract ([], "z4"),
-                                              QEpsilon "e2")))],
-                        SAbstract ([TInt], "z4"),
-                        QR "ra",
-                        [Isld (dummy_loc, "r1", 0);
-                         Iaop (dummy_loc, Add, "r1", "r1", UW (dummy_loc, WInt (dummy_loc, 1)));
-                         Isfree (dummy_loc, 1);
-                         Iret (dummy_loc, "ra", "r1")])))
-               ]) in
-  F.(ELam (dummy_loc, [("x", TInt)],
-           EApp (dummy_loc, EBoundary (dummy_loc, TArrow ([TInt], TInt),
-                            None,
-                            (TAL.(dummy_loc,[Iprotect (dummy_loc, [], "z2");
-                                   Imv (dummy_loc, "r1", UW (dummy_loc, WLoc (dummy_loc, l1)));
-                                   Ihalt (dummy_loc, FTAL.tytrans (TArrow ([TInt], TInt)),
-                                          SAbstract ([], "z2"),
-                                          "r1")], h))),
-                 [EVar (dummy_loc, "x")])))
-
+let blocks_2 = Parse.parse_string Parser.f_expression_eof {|
+lam (x:int).
+  FT[(int) -> int, ?]
+    ([protect , z2;
+      mv r1, l2;
+      halt
+        box forall[z4, e5].
+          {ra : box forall[].{r1 : int; z4} e5;
+           int :: z4} ra,
+        z2 {r1}],
+      [l2 -> box code [z3, e1]{ra : box forall[].{r1 : int; z3} e1;
+                              int :: z3} ra.
+               [sld r1, 0; add r1, r1, 1; sst 0, r1; jmp l3[z3, e1]],
+       l3 -> box code [z4, e2]{ra : box forall[].{r1 : int; z4} e2;
+                              int :: z4} ra.
+               [sld r1, 0; add r1, r1, 1; sfree 1; ret ra {r1}]])
+    x
+|}
 
 let higher_order =
   let tau = F.(TArrow([TArrow([TInt],TInt)], TInt)) in
@@ -256,55 +226,89 @@ let call_to_call =
 let ref_settyp = F.(TArrowMod ([TInt], [TAL.(TTupleRef [TInt])], [TAL.(TTupleRef [TInt])], TUnit))
 let ref_gettyp = F.(TArrowMod ([], [TAL.(TTupleRef [TInt])], [TAL.(TTupleRef [TInt])], TInt))
 
-let with_ref =
-  let ref_k = F.(TArrow ([ref_settyp; ref_gettyp], TInt)) in
-  let ftyp = F.(TArrow ([ref_settyp; ref_gettyp],TInt)) in
-  let stack = TAL.(SAbstract ([TTupleRef [TInt]; FTAL.tytrans ftyp], "z1")) in
-  F.(ELam (dummy_loc, [("init", TInt);
-            ("k", ref_k)],
-           EApp (dummy_loc, ELam (dummy_loc, [("_", TUnit);
-                        ("res", TInt);
-                        ("_", TUnit)],
-                      EVar (dummy_loc, "res")),
-                 [EBoundary (dummy_loc, TUnit, Some (TAL.(SAbstract ([TTupleRef [TInt]], "z"))), (TAL.(dummy_loc, [
-                      Iprotect (dummy_loc, [], "z");
-                      Isalloc (dummy_loc, 1);
-                      Iimport (dummy_loc, "r1", "z_", SAbstract ([], "z"), F.TInt, EVar (dummy_loc, "init"));
-                      Isst (dummy_loc, 0, "r1");
-                      Iralloc (dummy_loc, "r7", 1);
-                      Isalloc (dummy_loc, 1);
-                      Isst (dummy_loc, 0, "r7");
-                      Imv (dummy_loc, "r1", UW (dummy_loc, WUnit dummy_loc));
-                      Ihalt (dummy_loc, TUnit, SAbstract ([TTupleRef [TInt]], "z"), "r1")],
-                      [])));
-                  EApp (dummy_loc, EVar (dummy_loc, "k"),
-                        [ELamMod (dummy_loc, [("x", TInt)],
-                                  [TAL.(TTupleRef [TInt])],
-                                  [TAL.(TTupleRef [TInt])],
-                                  (EBoundary (dummy_loc, TUnit, Some stack,
-                                              TAL.(dummy_loc, [Isld (dummy_loc, "r1", 0);
-                                                    Iimport (dummy_loc, "r2", "z_",
-                                                             stack,
-                                                             F.TInt,
-                                                             F.EVar (dummy_loc, "x"));
-                                                    Ist (dummy_loc, "r1", 0, "r2");
-                                                    Imv (dummy_loc, "r1", UW (dummy_loc, WUnit dummy_loc));
-                                                    Ihalt (dummy_loc, TUnit, stack, "r1")], []))));
-                         ELamMod (dummy_loc, [],
-                                  [TAL.(TTupleRef [TInt])],
-                                  [TAL.(TTupleRef [TInt])],
-                                  (EBoundary (dummy_loc, TInt, Some stack,
-                                              TAL.(dummy_loc, [Isld (dummy_loc, "r1", 0);
-                                                    Ild (dummy_loc, "r2", "r1", 0);
-                                                    Ihalt (dummy_loc, TInt, stack, "r2")], []))))]);
-                  EBoundary (dummy_loc, TUnit, Some TAL.(SAbstract ([], "z")), (TAL.(dummy_loc, [
-                      Iprotect (dummy_loc, [TTupleRef [TInt]], "z");
-                      Isfree (dummy_loc, 1);
-                      Imv (dummy_loc, "r1", UW (dummy_loc, WUnit dummy_loc));
-                      Ihalt (dummy_loc, TUnit, SAbstract ([], "z"), "r1")],
-                      []
-                    )))])))
-
+let with_ref = Parse.parse_string Parser.f_expression_eof {|
+lam (init:int, k:((int)[ref <int>] -> [ref <int>]unit,
+                   ()[ref <int>] -> [ref <int>]int) -> int).
+  (lam (_:unit, res:int, _:unit). res)
+    FT[unit, ref <int> :: z]
+      ([protect , z;
+        salloc 1;
+        import r1, z_ as z, int TF{init};
+        sst 0, r1;
+        ralloc r7, 1;
+        salloc 1;
+        sst 0, r7;
+        mv r1, ();
+        halt unit, ref <int> :: z {r1}],
+        [])
+    (k
+      (lam [ref <int>][ref <int>](x:int).
+        FT[unit,
+           ref <int>
+             :: box forall[z12, e13].
+               {ra : box forall[].{r1 : int; z12} e13;
+                box forall[z14, e15].
+                    {ra : box forall[].{r1 : unit; ref <int> :: z14} e15;
+                     int :: ref <int> :: z14} ra
+                  :: box forall[z16, e17].
+                    {ra : box forall[].{r1 : int; ref <int> :: z16} e17;
+                     ref <int> :: z16} ra :: z12} ra :: z1]
+            ([sld r1, 0;
+              import
+                r2,
+                z_ as ref <int>
+                  :: box forall[z12, e13].
+                    {ra : box forall[].{r1 : int; z12} e13;
+                     box forall[z14, e15].
+                         {ra : box forall[].{r1 : unit; ref <int> :: z14} e15;
+                          int :: ref <int> :: z14} ra
+                       :: box forall[z16, e17].
+                         {ra : box forall[].{r1 : int; ref <int> :: z16} e17;
+                          ref <int> :: z16} ra :: z12} ra :: z1,
+                int TF{x};
+              st r1[0], r2;
+              mv r1, ();
+              halt
+                unit,
+                ref <int>
+                  :: box forall[z12, e13].
+                    {ra : box forall[].{r1 : int; z12} e13;
+                     box forall[z14, e15].
+                         {ra : box forall[].{r1 : unit; ref <int> :: z14} e15;
+                          int :: ref <int> :: z14} ra
+                       :: box forall[z16, e17].
+                         {ra : box forall[].{r1 : int; ref <int> :: z16} e17;
+                          ref <int> :: z16} ra :: z12} ra :: z1 {r1}],
+              []))
+      (lam [ref <int>][ref <int>]().
+        FT[int,
+           ref <int>
+             :: box forall[z12, e13].
+               {ra : box forall[].{r1 : int; z12} e13;
+                box forall[z14, e15].
+                    {ra : box forall[].{r1 : unit; ref <int> :: z14} e15;
+                     int :: ref <int> :: z14} ra
+                  :: box forall[z16, e17].
+                    {ra : box forall[].{r1 : int; ref <int> :: z16} e17;
+                     ref <int> :: z16} ra :: z12} ra :: z1]
+            ([sld r1, 0;
+              ld r2, r1[0];
+              halt
+                int,
+                ref <int>
+                  :: box forall[z12, e13].
+                    {ra : box forall[].{r1 : int; z12} e13;
+                     box forall[z14, e15].
+                         {ra : box forall[].{r1 : unit; ref <int> :: z14} e15;
+                          int :: ref <int> :: z14} ra
+                       :: box forall[z16, e17].
+                         {ra : box forall[].{r1 : int; ref <int> :: z16} e17;
+                          ref <int> :: z16} ra :: z12} ra :: z1 {r2}],
+              [])))
+    FT[unit, z]
+      ([protect ref <int>, z; sfree 1; mv r1, (); halt unit, z {r1}],
+        [])
+|}
 
 (* References *)
 
