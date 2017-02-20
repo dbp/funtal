c50916c44dbaa508bf7e08e761d976b7c912b868
Gabriel Scherer <gabriel.scherer@gmail.com>
2017-02-20 14:05:34 -0500

    improve the driver

--- driver.ml
+++ driver.ml
@@ -1,5 +1,36 @@
-let () =
-  let comp = Parse.parse_file Parser.component_eof (Sys.argv.(1)) in
+let print_component chan comp =
   let doc = Ftal.TALP.p_component comp in
-  PPrintEngine.ToChannel.pretty 0.8 80 stdout doc;
-  print_newline ()
+  PPrintEngine.ToChannel.pretty 0.8 80 chan doc;
+  output_string chan "\n";
+  flush chan
+
+let print_f_expression chan expr =
+  let doc = Ftal.FP.p_exp expr in
+  PPrintEngine.ToChannel.pretty 0.8 80 chan doc;
+  output_string chan "\n";
+  flush chan
+
+
+let handle_component_file path =
+  let comp = Parse.parse_file Parser.component_eof path in
+  print_component stdout comp
+
+let handle_f_expression_file path =
+  let expr = Parse.parse_file Parser.f_expression_eof path in
+  print_f_expression stdout expr
+
+let roundtrip_component comp =
+  let path = "tmp.ftal" in
+  let chan = open_out path in
+  print_component chan comp;
+  close_out chan;
+  handle_component_file path
+
+let roundtrip_f_expression expr =
+  let path = "tmp.ftal" in
+  let chan = open_out path in
+  print_f_expression chan expr;
+  close_out chan;
+  handle_f_expression_file path
+
+let () = handle_component_file Sys.argv.(1)
